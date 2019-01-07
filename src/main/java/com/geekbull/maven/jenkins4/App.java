41f43ab70d6779977d21a88f61fad0d9ce82bff5
srilaxmic <csrilaxmi154@gmail.com>
2019-01-07 15:29:19 +0530

    added buildtest.txt

---
+++ src/main/java/com/geekbull/maven/jenkins4/App.java
@@ -0,0 +1,13 @@
+package com.geekbull.maven.jenkins4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
